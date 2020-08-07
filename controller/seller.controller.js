59f5d4a1c3933e73a8930b4d98538313ed4bd435
tumoqun <32707186+tumoqun@users.noreply.github.com>
2020-08-07 20:36:51 +0700

    upload books

--- controller/seller.controller.js
+++ controller/seller.controller.js
@@ -1,26 +1,39 @@
 var userModel = require("../models/user");
 var bookModel=require("../models/book")
+const upload=require('../multer')
+const cloudinary=require('../cloudinary')
+const fs=require('fs')
 
 module.exports.postUpload = async (req, res) => {
     // const {user}=req
-    // const sellerId= await userModel.findById(user._id)
-    const { title, description, author, price, quantity, images, thumbnail  } = req.body;
-    console.log(images)
-    // // destructuring
-    // const Book= new bookModel({
-    //     title:title,
-    //     description:description,
-    //     author:author,
-    //     price:price,
-    //     quantity:quantity,
-    //     images:images,
-    //     thumbnail:thumbnail,
-    //     seller:sellerId
-    // })
-    // await Book.save(function (err) {
-    //     if (err) return handleError(err);
-    //     // saved!
-    //   });
-    // res.status(201).json({ success: true, Book });
+    const sellerId= await userModel.findById("5f28181d59ee352004b990b2")
+    const { title, description, author, price, quantity, thumbnail  } = req.body;
+    const uploader=async(path)=> await cloudinary.uploads(path,'images')
+    const urls=[]
+    const files=req.files
+    console.log(files)
+    for(const file of files){
+        const {path}=file
+        const newPath= await uploader(path)
+        urls.push(newPath)
+        fs.unlinkSync(path)
+    }
+    // destructuring
+    const Book= new bookModel({
+        title:title,
+        description:description,
+        author:author,
+        price:price,
+        quantity:quantity,
+        images:urls,
+        thumbnail:thumbnail,
+        seller:sellerId
+    })
+    await Book.save(function (err) {
+        if (err) return handleError(err);
+        // saved!
+      });
+    res.status(201).json({ success: true, Book });
 };
+
 
--- router/seller.router.js
+++ router/seller.router.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var router = express.Router();
 var sellerController = require("../controller/seller.controller");
-router.post("/upload", sellerController.postUpload);
+const upload=require("../multer")
+router.post("/upload",upload.array("images"), sellerController.postUpload);
 
 // router.post("/login", userController.postLogin);
 module.exports = router;
