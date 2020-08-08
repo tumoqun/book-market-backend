38be94f9c91e3531eb77df142ff386801be34aea
tumoqun <32707186+tumoqun@users.noreply.github.com>
2020-08-08 12:36:26 +0700

    fix conflict

--- models/user.js
+++ models/user.js
@@ -7,11 +7,8 @@ var userSchema = new mongoose.Schema({
     address: String,
     role: Number, // 1: buyer, 2: seller, 3: admin
     avatar: String,
-<<<<<<< HEAD
     name: String,
-=======
     comment:Array
->>>>>>> e120d8492470cfdbaee441c2e06fb17cb1a6cfd7
 });
 var User = mongoose.model("User", userSchema, "users");
 module.exports = User;
