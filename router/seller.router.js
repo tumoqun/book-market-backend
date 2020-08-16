216291406d86be3f55cef6e1d9372b06fe6a9ec8
a3chi <ninjaguyyyy@gmail.com>
2020-08-16 13:03:49 +0700

    nothing

--- router/seller.router.js
+++ router/seller.router.js
@@ -2,25 +2,25 @@ var express = require("express");
 var router = express.Router();
 const upload = require("../multer");
 const jwt = require("jsonwebtoken");
-const utils =require("../utils/utils")
+const utils = require("../utils/utils");
 
 function requireLogin(req, res, next) {
     let accessToken = req.header("Authorization");
     if (accessToken && accessToken.startsWith("Bearer ")) {
-      // Remove Bearer from string
-      accessToken = accessToken.slice(7, accessToken.length);
+        // Remove Bearer from string
+        accessToken = accessToken.slice(7, accessToken.length);
     }
-  
+
     jwt.verify(accessToken, "sercretKey", (err, decoded) => {
-      if (err) {
-        return res.status(401).send("Not authenticated");
-      }
-      req.user = decoded.user;
-      console.log(decoded)
-      req.authenticated = true;
-      return next();
+        if (err) {
+            return res.status(401).send("Not authenticated");
+        }
+        req.user = decoded.user;
+        console.log(decoded);
+        req.authenticated = true;
+        return next();
     });
-  }
+}
 var sellerController = require("../controller/seller.controller");
 
 // router.post("/upload", sellerController.postUpload);
@@ -29,7 +29,12 @@ var cpUpload = upload.fields([
     { name: "previewImgs", maxCount: 20 },
 ]);
 
-router.use(requireLogin)
-router.post("/upload",utils.requireRole(2), cpUpload, sellerController.postUpload);
+router.use(requireLogin);
+router.post(
+    "/upload",
+    utils.requireRole(2),
+    cpUpload,
+    sellerController.postUpload
+);
 
 module.exports = router;
--- utils/utils.js
+++ utils/utils.js
@@ -1,31 +1,31 @@
-class Utils{
-static requireRole(role) {
-    return (req, res, next) => {
-      // if (this.authenticate(req, res, next)) {
-      if (req.authenticated) {
-        switch (role) {
-          case 1:
-            if (req.user.role === role) {
-              return next();
+class Utils {
+    static requireRole(role) {
+        return (req, res, next) => {
+            // if (this.authenticate(req, res, next)) {
+            if (req.authenticated) {
+                switch (role) {
+                    case 1:
+                        if (req.user.role === role) {
+                            return next();
+                        }
+                        break;
+                    case 2:
+                        if (req.user.role === role) {
+                            return next();
+                        }
+                        break;
+                    case 3:
+                        if (req.user.role === role) {
+                            return next();
+                        }
+                        break;
+                    default:
+                        return res.status(403).send("403 Forbidden");
+                }
+                return res.status(403).send("403 Forbidden");
             }
-            break;
-          case 2:
-            if (req.user.role === role) {
-              return next();
-            }
-            break;
-          case 3:
-            if (req.user.role === role) {
-              return next();
-            }
-            break;
-          default:
             return res.status(403).send("403 Forbidden");
-        }
-        return res.status(403).send("403 Forbidden");
-      }
-      return res.status(403).send("403 Forbidden");
-    };
-  }
+        };
+    }
 }
 module.exports = Utils;
