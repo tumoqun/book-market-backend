c6bf442127581793dffc60ad15eea9a8c751ac48
a3chi <ninjaguyyyy@gmail.com>
2020-08-21 23:51:33 +0700

    for admin get orders

--- controller/admin.controller.js
+++ controller/admin.controller.js
@@ -8,18 +8,23 @@ var ObjectId = require("mongodb").ObjectId;
 const Cart = require("../models/cart");
 const { findById } = require("../models/user");
 const Book = require("../models/book");
+const Order = require("../models/order");
 var ObjectId = require("mongodb").ObjectId;
 
 module.exports.getUser = async (req, res) => {
-    const {role}=req.query
-    if(role==1){
-    const buyer=await User.find({role:1})
-    return res.json({success:true,data:buyer})
+    const { role } = req.query;
+    if (role == 1) {
+        const buyer = await User.find({ role: 1 });
+        return res.json({ success: true, data: buyer });
     }
-    if(role==2)
-    {
-        const seller=await User.find({role:2})
-    return res.json({success:true,data:seller})
+    if (role == 2) {
+        const seller = await User.find({ role: 2 });
+        return res.json({ success: true, data: seller });
     }
-    return res.status(404).json({success:false,data:"get user failed!"})
-}
+    return res.status(404).json({ success: false, data: "get user failed!" });
+};
+
+module.exports.getOrders = async (req, res) => {
+    const orders = await Order.find({});
+    res.json({ success: true, orders });
+};
--- router/admin.router.js
+++ router/admin.router.js
@@ -1,30 +1,30 @@
 var express = require("express");
 var router = express.Router();
-const utils =require("../utils/utils")
+const utils = require("../utils/utils");
 const jwt = require("jsonwebtoken");
-const adminController=require("../controller/admin.controller")
+const adminController = require("../controller/admin.controller");
 
 function requireLogin(req, res, next) {
     let accessToken = req.header("Authorization");
     if (accessToken && accessToken.startsWith("Bearer ")) {
-      // Remove Bearer from string
-      accessToken = accessToken.slice(7, accessToken.length);
+        // Remove Bearer from string
+        accessToken = accessToken.slice(7, accessToken.length);
     }
-  
+
     jwt.verify(accessToken, "sercretKey", (err, decoded) => {
-      if (err) {
-        return res.status(401).send("Not authenticated");
-      }
-      req.user = decoded.user;
-      console.log(decoded)
-      req.authenticated = true;
-      return next();
+        if (err) {
+            return res.status(401).send("Not authenticated");
+        }
+        req.user = decoded.user;
+        console.log(decoded);
+        req.authenticated = true;
+        return next();
     });
-  }
-//admin route here 
+}
+//admin route here
 
-router.use(requireLogin)
-router.get("/user-list", utils.requireRole(3),adminController.getUser);
+router.use(requireLogin);
+router.get("/user-list", utils.requireRole(3), adminController.getUser);
+router.get("/orders", utils.requireRole(3), adminController.getOrders);
 
-module.exports=router
-
+module.exports = router;
