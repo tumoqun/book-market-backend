232f7c67beefb62c0ab209b73ba34cad79cacd8b
a3chi <ninjaguyyyy@gmail.com>
2020-08-31 20:00:46 +0700

    fix dotenv for heroku

--- cloudinary.js
+++ cloudinary.js
@@ -1,24 +1,28 @@
-const cloudinary=require('cloudinary')
-const dotenv=require('dotenv')
+const cloudinary = require("cloudinary");
+const dotenv = require("dotenv");
 
-dotenv.config()
+require("dotenv").config({ silent: process.env.NODE_ENV === "production" });
 
 cloudinary.config({
-    cloud_name:process.env.CLOUD_NAME,
+    cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-})
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-exports.uploads=(file,folder)=>{
-    return new Promise(resolve=>{
-        cloudinary.uploader.upload(file,(result)=>{
-            resolve({
-                url: result.url,
-                id: result.public_id
-            })
-        },{
-            resource_type:"auto",
-            folder:folder
-        })
-    })
-}
+exports.uploads = (file, folder) => {
+    return new Promise((resolve) => {
+        cloudinary.uploader.upload(
+            file,
+            (result) => {
+                resolve({
+                    url: result.url,
+                    id: result.public_id,
+                });
+            },
+            {
+                resource_type: "auto",
+                folder: folder,
+            }
+        );
+    });
+};
