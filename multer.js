e39e199c1a991da6fe0bdb091431af4bf16d47f4
Torres Nguyen <torres.nguyen@starixvn.com>
2021-09-05 23:19:41 +0700

    update: multer and env file

--- multer.js
+++ multer.js
@@ -1,16 +1,16 @@
-const multer=require('multer')
+const multer = require("multer");
 
-const storage=multer.diskStorage({
-    destination: function(req,file,cb){
-        cb(null,'./uploads/')
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./uploads/");
     },
-    filename:function(req,file,cb){
-        cb(null,Date.now()+'-'+file.originalname)
-    }
-})
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname);
+    },
+});
 
-const upload=multer({
-    storage:storage,
-})
+const upload = multer({
+    storage: storage,
+});
 
-module.exports=upload
+module.exports = upload;
